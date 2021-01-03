ace70f334f045d7f57262cedf3c285c6b2943f2d
Jiseong Heo <jsheo@postech.ac.kr>
2021-01-04 00:00:34 +0900

    주식 종목 정보 db 연동 추가

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Stock(models.Model):
+
+#     id = models.AutoField(blank=True, null=True)
+    company = models.TextField(blank=True, null=True)
+    code = models.IntegerField(blank=True, null=True)
+    field = models.TextField(blank=True, null=True)
+    product = models.TextField(blank=True, null=True)
+    listing_date = models.TextField(blank=True, null=True)
+    settlement_month = models.TextField(blank=True, null=True)
+    representative = models.TextField(blank=True, null=True)
+    homepage = models.TextField(blank=True, null=True)
+    region = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'stock'
--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse, FileResponse
 from django.template import loader
-
+from .models import Stock
 
 def index(request):
     return render(request, 'stock/index.html')
@@ -16,3 +16,7 @@ def database(request):
 def db(request):
     response = FileResponse(open('/home/jsheo/AllN/news.db', 'rb'))
     return response
+
+def stock(request, number):
+    stock = get_object_or_404(Stock, code=number)
+    return render(request, 'stock/stock.html', {'stock':stock})
