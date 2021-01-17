1feed0a598e723da6210413be32dc85a0bad8ddc
DESKTOP-M8T3LO8\이형근 <lhk528@gmail.com>
2021-01-17 22:19:06 +0900

    index.html button 기능 활성화

---
+++ stock/static/js/unfold_fold.js
@@ -0,0 +1,26 @@
+var acc = window.document.querySelector(".news_tab");
+var news_wrapper = acc.nextElementSibling;
+
+acc.addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+    acc.classList.toggle("active");
+    
+//    document.getElemen
+    
+        /* Toggle between hiding and showing the active panel */
+//    if (news_wrapper.style.display === "block") {
+//        news_wrapper.style.display = "none";
+//    } else {
+//        news_wrapper.style.display = "block";
+//    }
+//    
+    
+    if (news_wrapper.style.maxHeight) {
+      news_wrapper.style.maxHeight = null;
+      acc.innerHTML = "펼치기";
+    } else {
+      acc.innerHTML = "접기";
+      news_wrapper.style.maxHeight = news_wrapper.scrollHeight + "px";
+    }
+});
